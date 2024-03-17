2c0c8119dddea9a55648107dc1cdde940654d80c
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-17 16:46:11 +0300

    add send message functionality

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxcPL3qCn3h2sNDucbxSQB6VCFh5uJ_88Yzpdy_mRiJWJg77mKDpzqB80cAUhboxRNH/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
