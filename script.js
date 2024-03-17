1437d9bfa89a310853995d536e91606d15627d8f
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-17 22:38:57 +0300

    add icons and logo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const tablinks = document.getElementsByClassName("tab-links");
 const tabcontents = document.getElementsByClassName("tab-contents");
+const imgElement = document.getElementById('main-image');
 function opentab(tabname){
     for(const tablink of tablinks){
         tablink.classList.remove("active-link");
@@ -9,6 +10,16 @@ function opentab(tabname){
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+
+    if(tabname == 'skills'){
+        imgElement.src = 'icons/Programming-amico.svg';
+    }
+    else if(tabname == 'experience'){
+        imgElement.src = 'icons/Version control-bro.svg';
+    }
+    else if(tabname == 'education'){
+        imgElement.src = 'icons/boy on graduation-amico.svg';
+    }
 }
 
 
